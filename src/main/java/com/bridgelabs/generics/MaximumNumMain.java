86a8670b00b97e58648bbbabf7b9e9640196714f
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-15 23:02:39 +0530

    Added message maximum number using generics on master branch

---
+++ src/main/java/com/bridgelabs/generics/MaximumNumMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabs.generics;
+
+public class MaximumNumMain {
+    public static void main(String[] args) {
+        System.out.println("Find Maximum number using Generics");
+    }
+}
